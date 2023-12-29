b769ad87d33d4e1178079d56ca7c43840a759ae2
Yan Slyfox <yan@Yans-MacBook-Pro.local>
2023-12-29 13:54:03 +0300

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ function App() {
 
     return (
       <div className='app_wrapper' >
-          <BrowserRouter>
+          <HashRouter>
                   <AppRouter/>
-          </BrowserRouter>
+          </HashRouter>
       </div>
   );
 }
