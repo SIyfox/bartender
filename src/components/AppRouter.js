6c607dc4c4c24503dec52a657576a2ce31a49b8f
Yan Slyfox <yan@Yans-MacBook-Pro.local>
2023-12-29 13:47:01 +0300

    setup gh-pages

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -3,19 +3,27 @@ import {Routes, Route, Navigate} from 'react-router-dom'
 
 import { publicRoutes } from '../routes';
 import {MENUPAGE_ROUTE,ORDERPAGE_ROUTE} from '../utils/consts';
+import BartenderHomePage from '../pages/BartenderMenuPage';
+import BartenderOrderPage from '../pages/BartenderOrderPage';
 
 const AppRouter = () => {
 
 
     return (
+
         <Routes>
+              <Route path="/menu" element={<BartenderHomePage />} />
+               <Route path="/orders" element={<BartenderOrderPage />} />
+    
+      </Routes>
+       // <Routes>
 
 
-            {publicRoutes.map(({path,Component})=> 
-                <Route key={path} path={path} element={<Component/>} exact/>
-            )}
-            <Route path="*" element={<Navigate replace to={MENUPAGE_ROUTE} />} />
-        </Routes>
+         //   {publicRoutes.map(({path,Component})=> 
+          //      <Route key={path} path={path} element={<Component/>} exact/>
+          //  )}
+        //    <Route path="*" element={<Navigate replace to={MENUPAGE_ROUTE} />} />
+       // </Routes>
     )
 }
 
