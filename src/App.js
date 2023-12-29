740587216b9de2eeb2a64f75da31b1553c0c6ddb
Yan Slyfox <yan@Yans-MacBook-Pro.local>
2023-12-29 13:54:55 +0300

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import {BrowserRouter} from 'react-router-dom'
 import AppRouter from "./components/AppRouter";
-
+import { HashRouter } from 'react-router-dom';
 function App() {
 
     return (
